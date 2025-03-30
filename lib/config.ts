4453305613dbeb37973e5bb67802771584674644
Dadzial <166065633+Dadzial@users.noreply.github.com>
2025-03-30 13:40:29 +0200

    Add files via upload

---
+++ lib/config.ts
@@ -0,0 +1,4 @@
+export const config = {
+    port: process.env.PORT || 3100,
+    database_url: process.env.MONGODB_URL || 'mongodb+srv://duszakdamian034:mLDvMF1fb6MV266Q@database.j5zi4um.mongodb.net/?retryWrites=true&w=majority&appName=DataBase'
+};
