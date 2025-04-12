27fc034cfb9bab9a904e132da50ed69207279b50
Dadzial <166065633+Dadzial@users.noreply.github.com>
2025-04-12 16:32:06 +0200

    Delete lib directory

--- lib/config.ts
+++
@@ -1,4 +0,0 @@
-export const config = {
-    port: process.env.PORT || 3100,
-    database_url: process.env.MONGODB_URL || 'mongodb+srv://duszakdamian034:mLDvMF1fb6MV266Q@database.j5zi4um.mongodb.net/?retryWrites=true&w=majority&appName=DataBase'
-};
